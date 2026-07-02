ff4172732c2a8ae9cd9c137933db2c3933b34e79
pato316 <pato316@gmail.com>
2026-07-02 21:52:39 +1000

    Version 3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taskboard-v1';
+const CACHE = 'taskboard-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
