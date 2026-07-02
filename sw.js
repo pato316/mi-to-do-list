b28db5b2e657723769c0b50a1ffdf939bee787c1
pato316 <pato316@gmail.com>
2026-07-02 22:31:11 +1000

    Version 5 - Move tasks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taskboard-v3';
+const CACHE = 'taskboard-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
