f01ee1382c2dc2c54f543066c8f349d50831d18b
pato316 <pato316@gmail.com>
2026-07-10 22:18:19 +1000

    Bump service worker cache version to v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taskboard-v24';
+const CACHE = 'taskboard-v25';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
