c91c449e958f791204024b765ecac8452b1f4c06
pato316 <pato316@gmail.com>
2026-07-10 21:33:41 +1000

    Bump service worker cache version to v23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taskboard-v22';
+const CACHE = 'taskboard-v23';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
